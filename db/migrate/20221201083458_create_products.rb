9de716b5fd6981cf1efbf5344fb54e8aa48d563c
jobelleborja <jobelleborja3@gmail.com>
2022-12-01 09:23:36 -0600

    Create tables

--- db/migrate/20221201083458_create_products.rb
+++ db/migrate/20221201083458_create_products.rb
@@ -1,7 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
-      t.string :name
+      t.integer :product_id
+      t.varchar :name
+      t.varchar :product_type
+      t.integer :price
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,18 @@
 require "open-uri"
-Products.destroy_all
-Product_Type.destroy_all
-Orders.destroy_all
-Users.destroy_all
+Product.destroy_all
+ProductType.destroy_all
 
 products = JSON.parse(URI.open("https://makeup-api.herokuapp.com/api/v1/products.json").read)
 
-# puts villagers["wol12"]["id"]
 i = 0;
 products.each do |product|
   if i < 100
-    gender = Gender.find_or_create_by(title: product["product_type"])
-      create_product= product_type.products.create(
+    product_type = ProductType.find_or_create_by(product_type: product_type["product_type"])
+      create_product = product_type.products.create(
         id: product["id"],
-        title: product["name"]
+        name: product["name"],
+        product_type: product["product_type"],
+        price: product["price"]
       )
       i+=1
   end
