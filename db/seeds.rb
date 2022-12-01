e768e4b22eaf053db068a154c33bde3323437be5
jobelleborja <jobelleborja3@gmail.com>
2022-12-01 02:35:30 -0600

    Create Product model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+Products.destroy_all
+Product_Type.destroy_all
+Orders.destroy_all
+Users.destroy_all
+
+products = JSON.parse(URI.open("https://makeup-api.herokuapp.com/api/v1/products.json").read)
+
+# puts villagers["wol12"]["id"]
+i = 0;
+products.each do |product|
+  if i < 100
+    gender = Gender.find_or_create_by(title: product["product_type"])
+      create_product= product_type.products.create(
+        id: product["id"],
+        title: product["name"]
+      )
+      i+=1
+  end
+end
+
