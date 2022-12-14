9b53a8f4ac4f612e640e6b5cb973af274e92f5a1
jobelleborja <jobelleborja3@gmail.com>
2022-12-14 12:06:27 -0600

    Merging Active Admin to Main
    Implement cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+<<<<<<< Updated upstream
+=======
+  before_action :initialize_session
+  helper_method :cart
+
+  private
+
+  def cart
+    # pass an array of product ids and get back collection of products
+    Product.find(session[:shopping_cart])
+  end
+
+  def initialize_session
+    # will initialize the visit count to zero for new users\
+    session[:shopping_cart] ||= [] # empty array of product IDs
+  end
+
+  def load_cart
+    # @cart = Product.find(session[:cart])
+    # Product.all.pluck(:id) => 1, 2
+    # Product.find(1, 2, 3) => error
+    # Product.where(id: [1, 2, 3]) => 1, 2
+    @cart = Product.where(id: session[:cart])
+  end
+>>>>>>> Stashed changes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -6,3 +7,20 @@ Rails.application.routes.draw do
 
 
 end
+=======
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  root "products#index"
+
+  get "/products", to: "products#index"
+  get "/about", to: "about#index"
+  resources :cart, only: %i[create destroy]
+  resources :checkout, only:[:create]
+  resources :products, only: [:index, :show] do
+    collection do
+      get "search"
+    end
+  end
+end
+>>>>>>> Stashed changes
